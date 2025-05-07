e3d7cc505788825c19cc774f61f87eabd0ee23b4
SaiSriKamalPeddinti <srikamalpeddinti10@gmail.com>
2025-05-07 04:06:07 +0000

    Repositioned imports

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './components/error/error.component';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 import { ManagerAddRequirementComponent } from './components/manager-add-requirement/manager-add-requirement.component';
 import { ManagerNavbarComponent } from './components/manager-navbar/manager-navbar.component';
 import { ManagerViewCandidateComponent } from './components/manager-view-candidate/manager-view-candidate.component';
 import { ManagerViewRequirementComponent } from './components/manager-view-requirement/manager-view-requirement.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { RecruiterAddCandidateComponent } from './components/recruiter-add-candidate/recruiter-add-candidate.component';
 import { RecruiterNavbarComponent } from './components/recruiter-navbar/recruiter-navbar.component';
 import { RecruiterViewCandidateComponent } from './components/recruiter-view-candidate/recruiter-view-candidate.component';
 import { RecruiterViewRequirementComponent } from './components/recruiter-view-requirement/recruiter-view-requirement.component';
 import { RecruiterViewRegistrationComponent } from './components/recruiter-view-registration/recruiter-view-registration.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
