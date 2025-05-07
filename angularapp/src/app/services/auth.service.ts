c8f32eacca0c020820dbad31ac5ea6369e4e2e4b
SaiSriKamalPeddinti <srikamalpeddinti10@gmail.com>
2025-05-07 04:15:21 +0000

    Removed pipe logic from login method

--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
-import { catchError} from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { Login } from '../models/login.model';
 import { environment } from 'src/environments/environment';
@@ -12,46 +12,27 @@ import { environment } from 'src/environments/environment';
 })
 export class AuthService {
 
-  private currentRoleSubject=new BehaviorSubject<string | null>(null);
-  private currentUserSubject=new BehaviorSubject<string | null>(null);
+  private userSubject = new BehaviorSubject<User | null>(null);
+  user$ = this.userSubject.asObservable();
 
-  constructor(private http:HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   register(user: User): Observable<any> {
     return this.http.post<any>(`${environment.backendUrl}/user/signup`, user);
   }
 
-
   login(loginData: Login): Observable<any> {
-    return this.http.post<any>(`${environment.backendUrl}/user/login`, loginData).pipe(
-      catchError(error => {
-        let errorMsg = 'An error occurred';
-        if (error.error instanceof ErrorEvent) {
-          errorMsg = `Error: ${error.error.message}`;
-        } else {
-          errorMsg = error.error?.message || 'Invalid credentials';
-        }
-        return throwError(() => new Error(errorMsg));
-      })
-    );
+    return this.http.post<any>(`${environment.backendUrl}/user/login`, loginData);
   }
 
-
-  logout():void{
+  logout(): void {
     localStorage.clear();
-    this.currentUserSubject.next(null);
-    this.currentRoleSubject.next(null);
+    this.userSubject.next(null);
     this.router.navigate(['/login']);
   }
-  isAuthenticated():boolean{
+
+  isAuthenticated(): boolean {
     return !!localStorage.getItem('token');
   }
-  
 
-  getAllUsers():Observable<User[]>{
-    const headers=new HttpHeaders({
-      Authorization:`Bearer ${localStorage.getItem('token')}`
-    });
-    return this.http.get<User[]>(`${environment.backendUrl}/user/getAllusers`,{ headers });
-  }
 }
