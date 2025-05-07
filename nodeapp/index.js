67d36bfd08cc8c3f30ee52b0d5c0a9f9021e4a12
SaiSriKamalPeddinti <srikamalpeddinti10@gmail.com>
2025-05-07 04:10:18 +0000

    Updated index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,33 +1,27 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const userRouter = require('./routers/userRouter');
+const app = express();
 
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors(
-    {
-        origin: 'https://8081-accecafecdeea325967471cfdbcbacfdone.premiumproject.examly.io',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        exposedHeaders: ['Content-Type', 'X-Powered-By'],
-        credentials: false
-    }
-))
+app.use(cors({
+    origin: 'https://8081-accecafecdeea325967471cfdbcbacfdone.premiumproject.examly.io',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Type', 'X-Powered-By'],
+    credentials: false
+}))
 
 app.use('/user', userRouter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/HireFlow', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-    .then(() => {
-        console.log('Connected to database');
-        app.listen(8080, () => {
-            console.log('Running on port 8080');
-        })
+}).then(() => {
+        app.listen(8080);
     })
     .catch((error) => {
         console.log('Error occurred: ', error.stack);
