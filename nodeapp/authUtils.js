fccb07f3fb0f4d86f620a2e68a5f0edd6b45565f
ChanduS33 <chandusai754@gmail.com>
2025-05-08 08:48:43 +0000

    TO pull the code from the main to pass the test cases

--- nodeapp/authUtils.js
+++ nodeapp/authUtils.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config();
 
-exports.generateToken = (userId) => jwt.sign({userId}, SECRET_KEY, { expiresIn: '1h' })
+exports.generateToken = (userId) => jwt.sign({userId}, process.env.SECRET_KEY, { expiresIn: '1h' })
 
 exports.validateToken = (req, res, next) => {
     try {
-        const authHeader = req.header.authorization
+        const authHeader = req.header('Authorization');
         if (!authHeader || !authHeader.startsWith('Bearer')) {
             return res.status(400).json({ message: "Authentication failed" })
         }
-        const token = authHeader.subString(7)
-        jwt.verify(token, SECRET_KEY);
+        const token = authHeader.substring(7)
+        jwt.verify(token, process.env.SECRET_KEY);
         next()
     } catch (error) {
         res.status(500).json({ error: error.message })
--- nodeapp/routers/candidateRouter.js
+++ nodeapp/routers/candidateRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { addCandidate, deleteCandidate, getAllCandidates, getCandidateById, updateCandidate } = require('../controllers/candidateController');
-
-router.get('/getAllCandidates', getAllCandidates)
-router.get('/getCandidateById/:id', getCandidateById);
-router.post('/addCandidate', addCandidate);
-router.put('/updateCandidate/:id', updateCandidate);
-router.delete('/deleteCandidate/:id', deleteCandidate);
+const { validateToken } = require('../authUtils');
+router.get('/getAllCandidates', validateToken, getAllCandidates)
+router.get('/getCandidateById/:id', validateToken, getCandidateById);
+router.post('/addCandidate', validateToken, addCandidate);
+router.put('/updateCandidate/:id', validateToken, updateCandidate);
+router.delete('/deleteCandidate/:id', validateToken, deleteCandidate);
 
 module.exports = router;
